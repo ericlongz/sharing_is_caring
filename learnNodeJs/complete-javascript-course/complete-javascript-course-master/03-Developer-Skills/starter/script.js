ab9305a3fb1fdadf32f3229bff31d9b58154d7e2
Eric <ericlongz@ymail.com>
2023-01-08 20:01:10 +0700

    commit learning

--- learnNodeJs/complete-javascript-course/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ learnNodeJs/complete-javascript-course/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+let myVar = 'test';
+
+//TODO
+//BUG
--- learnNodeJs/complete-javascript-course/script/loopCodingChallenge#4.js
+++ learnNodeJs/complete-javascript-course/script/loopCodingChallenge#4.js
@@ -1,20 +1,20 @@
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
 const calcTip = (bills) => {
-  let tips = [];
-  let total = [];
-  for (let i = 0; i < bills.length; i++) {
-    if ((bills[i] >= 50) & (bills[i] <= 300)) {
-      tipsValue = 0.15 * bills[i];
-      tips.push(tipsValue);
-      total.push(bills[i] + tipsValue);
-    } else {
-      tipsValue = 0.2 * bills[i];
-      tips.push(tipsValue);
-      total.push(bills[i] + tipsValue);
-    }
-  }
-  return [total, tips];
+	let tips = [];
+	let total = [];
+	for (let i = 0; i < bills.length; i++) {
+		if ((bills[i] >= 50) & (bills[i] <= 300)) {
+			tipsValue = 0.15 * bills[i];
+			tips.push(tipsValue);
+			total.push(bills[i] + tipsValue);
+		} else {
+			tipsValue = 0.2 * bills[i];
+			tips.push(tipsValue);
+			total.push(bills[i] + tipsValue);
+		}
+	}
+	return [total, tips];
 };
 
 let total = calcTip(bills)[0];
@@ -25,11 +25,11 @@ console.log(tips);
 console.log(total);
 
 const calcAverage = (arr) => {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
-  }
-  return sum / arr.length;
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum = sum + arr[i];
+	}
+	return sum / arr.length;
 };
 
 console.log(calcAverage(total));
