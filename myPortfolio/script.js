af32dd75cc1cb48ab9ce80cd296716214472b674
Eric <ericlongz@ymail.com>
2023-07-19 19:19:09 +0700

    update

---
+++ myPortfolio/script.js
@@ -0,0 +1,28 @@
+// function strictFunc() {
+// 	"use strict";
+// 	undeclaredVar = 123;
+// }
+// strictFunc();
+
+// console.log(undeclaredVar);
+
+// const arr = Object.keys({ a: 1, b: 2, c: 3 });
+// [0, 1, 2].forEach((x) => console.log(arr[x]));
+
+// function foo() {}
+// const foo = function () {};
+
+// const foo = function () {};
+
+// const a = (b = 3);
+
+// console.log(a);
+
+let x = 1;
+
+if (x < 0)
+	function foo() {
+		console.log("ok");
+	}
+
+foo();
