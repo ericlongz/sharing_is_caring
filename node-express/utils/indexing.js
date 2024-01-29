95d5ac5ff9b94964f50b16a27a3d4992e2dc45c4
ERIC <U070048@bca.co.id>
2024-01-29 09:57:47 +0700

    delete node-express

--- node-express/utils/indexing.js
+++
@@ -1,3 +0,0 @@
-const uri = "mongodb://autoweb:devautoweb@devnosqldb01,devnosqldb02,devnosqldb03:28003/?authSource=dautoweb&replicaSet=rsdev2&readPreference=primary&tls=false";
-
-db.statistic.createIndex(JOB_MEM_NAME : 1);
