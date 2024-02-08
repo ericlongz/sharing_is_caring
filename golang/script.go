de532bf7b21b6da6f557f7c32e813a532256178c
Eric Wu <ericwu@Erics-MacBook-Air.local>
2024-02-08 22:21:10 +0700

    learn golang

--- golang/script.go
+++ golang/script.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// contoh command inline
+/* contoh command multiline
+ini adalah contoh command multiline
+*/
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println("Please enter your first name.")
+	var firstName string
+	fmt.Scanln(&firstName)
+	fmt.Println("Please enter your last name.")
+	var lastName string = fmt.Scanln(&lastName)
+	fmt.Printf("Hi, %s %s! I'm Go!", firstName, lastName)
 }
